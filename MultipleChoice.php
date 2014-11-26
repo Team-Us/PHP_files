94bac34060c30b01f8b12e6e9208d468b1e9bdbc
NickChatelain <i.am.honeyhead@gmail.com>
2014-11-26 15:51:06 -0700

    Exercises are now entirely dynamically populated. The index.php contains several forms, which are infact buttons disguised as links for navigation. Once data is in database, then these links will be used for the main menu and the navigation box on the left hand side of the exercises.

--- MultipleChoice.php
+++ MultipleChoice.php
@@ -8,37 +8,47 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="./dnd_css.css"/>
     </head>
     <body>
         
      <?php
         include 'exerciseFunctions.php';
-        $_POST["eID"]=1;
+        
         $exercise=$_POST["eID"];
-        //functions all included in exerciseFunctions.php
         
- 
+        $dbConnected=connectToDatabase();
         
-        $dbConnected=  connectToDatabase();
-        $prompt=getPromptData($exercise,$dbConnected);
         $options=  getMultipleChoiceOptions($exercise, $dbConnected); 
-          
+        
+        $prompt=getPromptData($exercise,$dbConnected);
+        
+        function checkSet($option)
+        {
+            if(isset($_POST['userEntry']) && $_POST['userEntry']==$option)
+            {
+                echo ' checked="true"';
+            }
+        }
         ?>
         <!-- form is created, and dynamically populated with the data retrieved from the database, and it posts to this same page-->
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             Multiple Choice:
-            <br><?php echo($prompt["Prompt"]);?><br>
-            <?php echo ($options['OptionA'])?><br>
-            <input type="radio" name="userEntry"  value="a">a. <?php echo ($options['OptionA'])?><br>
-            <input type="radio" name="userEntry"  value="b">b. <?php echo ($options['OptionB'])?><br>
-            <input type="radio" name="userEntry"  value="c">c. <?php echo ($options['OptionC'])?><br>
-            <input type="radio" name="userEntry"  value="d">d. <?php echo ($options['OptionD'])?><br>
+            <br>
+            <?php echo($prompt["Prompt"]);?>
+            <br>
+            <input type="radio" name="userEntry"  value="a" <?php checkSet("a")?>>a. <?php echo ($options['OptionA'])?><br>
+            <input type="radio" name="userEntry"  value="b" <?php checkSet("b")?>>b. <?php echo ($options['OptionB'])?><br>
+            <input type="radio" name="userEntry"  value="c" <?php checkSet("c")?>>c. <?php echo ($options['OptionC'])?><br>
+            <input type="radio" name="userEntry"  value="d" <?php checkSet("d")?>>d. <?php echo ($options['OptionD'])?><br>
+            <input type="hidden" name="eID" value="<?php echo $exercise?>">
+            
             <input type="submit" name="submit" value="Submit" >
         </form>
        
       <?php
-            checkResponse($prompt['answer'],$_POST['userEntry'] );
-            mysql_close($dbConnected);
+            checkResponse($prompt['Answer'],$_POST['userEntry'] );
+            
        ?><!-- end php post entry-->
         
         
